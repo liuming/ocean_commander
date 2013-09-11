2f544899bd1c3c6de9005064a6dca22741ef74fe
Ming Liu <liuming@lmws.net>
2013-09-10 17:49:39 -0700

    ssh keys management commands

--- lib/ocean_commander/ssh_keys.rb
+++ lib/ocean_commander/ssh_keys.rb
@@ -1,10 +1,28 @@
 module OceanCommander
   class SshKeys < General
-    default_task :list
-
-    desc "ssh_keys", "list ssh keys"
+    desc "list", "list ssh keys"
     def list
       tp api.ssh_keys.list.ssh_keys
+    end
+
+    desc "show SSH_KEY_ID", "show an ssh key"
+    def show(id=nil)
+      tp api.ssh_keys.show(id).ssh_key
+    end
+
+    desc "new NAME SSH_PUB_KEY", "create new ssh public key"
+    def new(name,ssh_pub_key=nil)
+      tp api.ssh_keys.add(name: name, ssh_pub_key: ssh_pub_key).ssh_key
+    end
+
+    desc "edit SSH_KEY_ID SSH_PUB_KEY", "edit an ssh key"
+    def edit(id, ssh_pub_key=nil)
+      tp api.ssh_keys.edit(id, ssh_pub_key: ssh_pub_key).ssh_key
+    end
+
+    desc "destroy", "delete an ssh public key"
+    def destroy(id)
+      tp api.ssh_keys.delete(id).ssh_key
     end
   end
 end
