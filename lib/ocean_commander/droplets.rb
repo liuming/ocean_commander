a4b2695eb2b9cfb85499374f354886aa19ed6bad
Ming Liu <liuming@lmws.net>
2013-10-11 17:43:52 -0700

    create and delete droplet

--- lib/ocean_commander/droplets.rb
+++ lib/ocean_commander/droplets.rb
@@ -4,5 +4,15 @@ module OceanCommander
     def list
       tp api.droplets.list.droplets
     end
+
+    desc "create NAME SIZE_ID IMAGE_ID REGION_ID", "create a droplet"
+    def create(name, size, image, region, ssh_keys='')
+      tp api.droplets.create(name: name, size_id: size, image_id: image, region_id: region, ssh_key_ids: ssh_keys).droplet
+    end
+
+    desc "delete ID", "delete a droplet"
+    def delete(id)
+      tp api.droplets.delete(id)
+    end
   end
 end
