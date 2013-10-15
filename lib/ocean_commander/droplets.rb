1d3d473d5ddd5810dbb42e95a5068f53a51f2e93
Ming Liu <liuming@lmws.net>
2013-10-15 16:14:10 -0700

    some basic commands for droplets

--- lib/ocean_commander/droplets.rb
+++ lib/ocean_commander/droplets.rb
@@ -4,7 +4,6 @@ module OceanCommander
     def list
       tp api.droplets.list.droplets
     end
-
     desc "create NAME SIZE_ID IMAGE_ID REGION_ID", "create a droplet"
     def create(name, size, image, region, ssh_keys='')
       tp api.droplets.create(name: name, size_id: size, image_id: image, region_id: region, ssh_key_ids: ssh_keys).droplet
@@ -13,6 +12,41 @@ module OceanCommander
     desc "delete ID", "delete a droplet"
     def delete(id)
       tp api.droplets.delete(id)
+    end
+
+    desc "show", "show a droplet"
+    def show(id)
+      tp api.droplets.show(id).droplet
+    end
+
+    desc "reboot", "reboot a droplet"
+    def reboot(id)
+      tp api.droplets.reboot(id).droplet
+    end
+
+    desc "shutdown", "shutdown a droplet"
+    def shutdown(id)
+      tp api.droplets.shutdown(id)
+    end
+
+    desc "poweron", "turn on a droplet"
+    def poweron(id)
+      tp api.droplets.power_on(id)
+    end
+
+    desc "poweroff", "turn off a droplet"
+    def poweroff(id)
+      tp api.droplets.power_off(id)
+    end
+
+    desc "rebuild", "rebuild a droplet"
+    def rebuild(id, image_id)
+      tp api.droplets.rebuild(id, image_id: image_id)
+    end
+
+    desc "password_reset", "reset password for a droplet"
+    def password_reset(id, image_id)
+      tp api.droplets.password_reset(id)
     end
   end
 end
